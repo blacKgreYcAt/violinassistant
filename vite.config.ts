e9cdc7b1628d344fbecc8ccfeeba9ccbb6dc35a3
blacKgreYcAt <ksbd7zs2yj@privaterelay.appleid.com>
2026-03-13 12:04:55 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,31 +1,14 @@
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
-import {defineConfig, loadEnv} from 'vite';
-
-export default defineConfig(({mode}) => {
-  const env = loadEnv(mode, '.', '');
-  return {
-    base: './',
-    plugins: [
-      react(), 
-      tailwindcss(),
-    ],
-    server: {
-      hmr: process.env.DISABLE_HMR !== 'true',
-    },
-  };
-});
-import tailwindcss from '@tailwindcss/vite';
-import react from '@vitejs/plugin-react';
-import {defineConfig, loadEnv} from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import { VitePWA } from 'vite-plugin-pwa';
 
-export default defineConfig(({mode}) => {
+export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
   return {
     base: './',
     plugins: [
-      react(), 
+      react(),
       tailwindcss(),
       VitePWA({
         registerType: 'autoUpdate',
