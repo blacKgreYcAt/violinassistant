1ce3af49f3e2214a20c03b814faf1fde9f950f55
blacKgreYcAt <ksbd7zs2yj@privaterelay.appleid.com>
2026-03-13 10:52:07 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,7 @@ export default defineConfig(({mode}) => {
     plugins: [
       react(), 
       tailwindcss(),
+      /*
       VitePWA({
         registerType: 'autoUpdate',
         includeAssets: [],
@@ -31,6 +32,7 @@ export default defineConfig(({mode}) => {
           ]
         }
       })
+      */
     ],
     server: {
       hmr: process.env.DISABLE_HMR !== 'true',
